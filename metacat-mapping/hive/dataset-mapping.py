597d26b3059b44d98c10f61e23a6a309e410b61c
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2025-09-25 17:18:48 +0100

    First draft of mapping script.

--- metacat-mapping/hive/dataset-mapping.py
+++ metacat-mapping/hive/dataset-mapping.py
@@ -1,3 +1,30 @@
 # Read in pulse json file
+import json
 
-# Map data to dataset schema using dataset.jq
+import jq
+import requests
+from jsonschema import validate, ValidationError
+
+
+with open("pulse.json") as file:
+    pulse_data = json.load(file)
+
+# Map data to ukaea-dataset schema using dataset.jq
+jq_url = "https://raw.githubusercontent.com/ukaea/ukaea-metadata/main/metacat-mapping/hive/dataset.jq"
+jq_spec = requests.get(jq_url).text
+
+hive_dataset = jq.compile(jq_spec).input(pulse_data).first()
+
+# Query scicat using experimentID to get contactEmail, ownerGroup and owner
+hive_dataset["contactEmail"] = "<contactEmail>"
+hive_dataset["ownerGroup"] = "<ownerGroup>"
+hive_dataset["owner"] = "<owner>"
+
+# Validate against ukaea-dataset.schema.json
+schema_url = "https://raw.githubusercontent.com/ukaea/ukaea-metadata/main/ukaea-schema/ukaea-dataset.schema.json"
+schema = requests.get(schema_url).json()
+
+try:
+    validate(instance=hive_dataset, schema=schema)
+except ValidationError as e:
+    print(f"Validation error: {e.message}")
