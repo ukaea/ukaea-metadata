7eea1c81c1cea9ce29e333937595d3fdebd25cdc
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2025-10-17 17:04:18 +0100

    Refactor instrument mapping to loop through equipment directory and read multiple JSON files

--- metacat-mapping/hive/instrument-mapping.py
+++ metacat-mapping/hive/instrument-mapping.py
@@ -1,27 +1,30 @@
 import json
+from pathlib import Path
 
 import jq
 import requests
 from jsonschema import validate, ValidationError
 
-# TODO: Loop through equipment directory
-# Read in equipment json file
-with open("equipment.json") as file:
-    instrument_data = json.load(file)
+equipment_dir = Path("equipment")
 
-# Map data to ukaea-dataset schema using dataset.jq
-jq_url = "https://ukaea.github.io/ukaea-metadata/metacat-mapping/hive/instrument.jq"
-jq_spec = requests.get(jq_url).text
+for equipment_file in equipment_dir.glob("*.json"):
+    # Read in equipment json file
+    with open(equipment_file) as file:
+        instrument_data = json.load(file)
 
-hive_instrument = jq.compile(jq_spec).input(instrument_data).first()
+    # Map data to ukaea-instrument schema using instrument.jq
+    jq_url = "https://ukaea.github.io/ukaea-metadata/metacat-mapping/hive/instrument.jq"
+    jq_spec = requests.get(jq_url).text
 
-# Validate against ukaea-dataset.schema.json
-schema_url = (
-    "https://ukaea.github.io/ukaea-metadata/ukaea-schema/ukaea-instrument.schema.json"
-)
-schema = requests.get(schema_url).json()
+    hive_instrument = jq.compile(jq_spec).input(instrument_data).first()
 
-try:
-    validate(instance=hive_instrument, schema=schema)
-except ValidationError as e:
-    print(f"Validation error: {e.message}")
+    # Validate against ukaea-instrument.schema.json
+    schema_url = (
+        "https://ukaea.github.io/ukaea-metadata/ukaea-schema/ukaea-instrument.schema.json"
+    )
+    schema = requests.get(schema_url).json()
+
+    try:
+        validate(instance=hive_instrument, schema=schema)
+    except ValidationError as e:
+        print(f"Validation error: {e.message}")
