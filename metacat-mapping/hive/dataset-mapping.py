45d534563a7b55ea84b32cbad31b81faef79ffb5
khalid <khalid619lawal@gmail.com>
2025-10-31 08:56:54 +0000

    fix: change diagnoticsID to diagnosticsId in dataset-mapping.py

--- metacat-mapping/hive/dataset-mapping.py
+++ metacat-mapping/hive/dataset-mapping.py
@@ -42,8 +42,8 @@ for dataset in hive_pulse:
 
     output_file = (
         pulse_directory
-        / Path(f"{dataset['diagnosticID']}")
-        / f"{dataset['diagnosticID']}-metadata.json"
+        / Path(f"{dataset['diagnosticId']}")
+        / f"{dataset['diagnosticId']}-metadata.json"
     )
 
     output_file.parent.mkdir(parents=True, exist_ok=True)
